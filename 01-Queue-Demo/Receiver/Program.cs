bf4a5b203231c30ee7db0f2f9ebb1b39ff42c13d
Bishoy Demian <eng.bishoy@gmail.com>
2014-09-04 15:26:04 +1000

    added queue sender and receiver demo

---
+++ 01-Queue-Demo/Receiver/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Messages;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListenToMessages();
+        }
+
+        private static void ListenToMessages()
+        {
+            var connectionString =
+                CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+
+            var client =
+                QueueClient.CreateFromConnectionString(connectionString, "TestQueue");
+
+            while (true)
+            {
+                var message = client.Receive();
+
+                if (message != null)
+                {
+                    try
+                    {
+                        var payload = message.GetBody<NewUserRegistered>();
+                        Console.WriteLine("MessageID: " + message.MessageId);
+                        Console.WriteLine("User id #{0} name: {1}", payload.Id, payload.FullName);
+
+                        // Remove message from queue
+                        message.Complete();
+                    }
+                    catch
+                    {
+                        // Indicate a problem, unlock message in queue
+                        message.Abandon();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 01-Queue-Demo/Sender/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Messages;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendMessages();
+            Console.ReadKey();
+        }
+
+        private static void SendMessages()
+        {
+            var connectionString =
+                CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+
+            var client =
+                QueueClient.CreateFromConnectionString(connectionString, "TestQueue");
+
+            client.Send(new BrokeredMessage(new NewUserRegistered{Id = 1, FullName = "User 01"}));
+        }
+    }
+}
